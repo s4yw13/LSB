8dd1ba74a81d2587df8bae02324897cc05c0f4b6
Lain <user8174d39d@mail.ru>
2025-03-24 02:09:43 +0300

    Visual extraction of Least Significant Bits of an image

---
+++ demo/merge_channels.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+red_img = Image.open("red_channel_output.png")
+green_img = Image.open("green_channel_output.png")
+blue_img = Image.open("blue_channel_output.png")
+
+width, height = red_img.size
+
+final_img = Image.new('RGB', (width, height), (0, 0, 0))
+
+for x in range(width):
+    for y in range(height):
+        r_pixel = red_img.getpixel((x, y))[0]
+        g_pixel = green_img.getpixel((x, y))[0]
+        b_pixel = blue_img.getpixel((x, y))[0]
+
+        pixel_color = (r_pixel * 255, g_pixel * 255, b_pixel * 255)
+        final_img.putpixel((x, y), pixel_color)
+
+final_img.save("merged_image.png")
---
+++ demo/split_channels.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+
+image = Image.open("image.png")
+
+width, height = image.size
+
+
+def get_lsb(value):
+    return value & 1
+
+
+def process_channel(channel_data, color):
+    result_data = []
+    for pixel in channel_data:
+        binary_value = bin(pixel)[-1]
+        value = int(binary_value) * 255
+        result_data.append((value, value, value))
+
+    result_img = Image.new('RGB', (width, height), color)
+    result_img.putdata(result_data)
+    return result_img
+
+
+pixels = list(image.getdata())
+
+r_channel = [pixel[0] for pixel in pixels]
+g_channel = [pixel[1] for pixel in pixels]
+b_channel = [pixel[2] for pixel in pixels]
+
+new_r_img = process_channel(r_channel, (255, 0, 0))
+new_g_img = process_channel(g_channel, (0, 255, 0))
+new_b_img = process_channel(b_channel, (0, 0, 255))
+
+new_r_img.save('red_channel_output.png')
+new_g_img.save('green_channel_output.png')
+new_b_img.save('blue_channel_output.png')
